66b77f792cd9fdc1dad83af3f26523cfcefc8df1
Holyepitaph <116224843+Holyepitaph@users.noreply.github.com>
2023-08-13 11:54:13 -0700

    Base with Framer Motion

---
+++ app/app/page.js
@@ -0,0 +1,11 @@
+"use client"
+import {motion} from 'framer-motion'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    
+    </main>
+  )
+}
